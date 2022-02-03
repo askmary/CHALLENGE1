1c93435c6a97001f1a60b7b188caac0250d9d89b
Mariana Melo <93939350+askmary@users.noreply.github.com>
2022-02-03 20:36:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Challenge extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Olá, bem vindo.</h1>
+      </div>
+    );
+  }
+}
+export default Challenge;
